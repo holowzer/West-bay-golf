bd9f793d67b76a7cbe497ea898d7c0b1ef511694
holowzer <arthurlasnelhetic@gmail.com>
2020-01-09 11:14:57 +0100

    bla

---
+++ src/compfolder/globalComp/Slider.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+ 
+
+ 
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: false,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  }
+}
+ 
+const Slideshow = (props) => {
+
+
+    const slideImages = [
+        props.img0,
+        props.img1,
+        props.img2
+      ];
+    return (
+      <div className="slide-container">
+        <Slide {...properties}>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+            </div>
+          </div>
+        </Slide>
+      </div>
+    )
+}
+
+export default Slideshow
---
+++ src/compfolder/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import img0 from "../images/download0.jpg"
+import img1 from "../images/download1.jpg"
+import img2 from "../images/download2.jpg"
+import Slideshow from './globalComp/Slider';
+
+
+let Homepage = ()=>{
+
+    return(
+        <Slideshow img0={img0} img1={img1}  img2={img2} />
+    )
+}
+
+export default Homepage
