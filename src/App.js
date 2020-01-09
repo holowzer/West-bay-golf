b7ec4b6da7d4c2301252d963b2e574df1b6fc2d2
holowzer <arthurlasnelhetic@gmail.com>
2020-01-10 00:07:23 +0100

    router addition

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import Golfcourse from "./compfolder/Golfcourse"
 import Homepage from "./compfolder/Homepage"
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
+
+    <Router>
     <div className="App">
-      <Homepage/>
+      <Route path="/Homepage" component={Homepage}/>
+      <Route path="/Golfcourse" component={Golfcourse}/>
     </div>
+    </Router>
   )
 }
 
