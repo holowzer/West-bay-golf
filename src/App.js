5f55af5e381f7b1b1f2dbe878edf92938962bff4
Joshua <satwa.fr@gmail.com>
2020-01-10 11:18:35 +0100

    Contact form (front-end) done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Homepage from "./compfolder/Homepage"
 import Golfcourse from "./compfolder/Golfcourse"
 
 import BookGolf from './compfolder/globalComp/contactPages/BookGolf'
+import SalesContact from './compfolder/globalComp/contactPages/SalesContact'
+import RentalContact from './compfolder/globalComp/contactPages/RentalContact'
 
 import Aboutus from "./compfolder/Aboutus"
 import Footer from "./compfolder/globalComp/Footer"
@@ -21,6 +23,8 @@ function App() {
         <Route path="/about-us" component={Aboutus}/>
 
         <Route path="/contact/book-golf" component={BookGolf}/>
+        <Route path="/contact/real-estate-sales" component={SalesContact}/>
+        <Route path="/contact/real-estate-rental" component={RentalContact}/>
       </div>
       <Footer/>
     </Router>
--- src/compfolder/globalComp/contactPages/RentalContact.jsx
+++ src/compfolder/globalComp/contactPages/RentalContact.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import westBayLogo from "../../../images/LogowestBay.png"
 import img1 from "../../../images/download1.jpg"
 import "./GlobalContact.scss"
 import Nav from "../../globalComp/Nav"
 
-const BookGolf = () => {
+const RentalContact = () => {
     return (
         <div className="contactContainer">
             <div>
                 <Nav />
-                <img src={img1} alt="Navigation" className="nav-image-short"/>
+                <img src={img1} alt="Navigation" className="nav-image-short" />
             </div>
 
             <h1>We Look Forward to Connecting Soon!</h1>
@@ -22,14 +21,16 @@ const BookGolf = () => {
 
                 <div className="contact-form-block">
                     <div className="company-info">
-                        <img src="/blackiguana-logo.png" alt="Black Iguana logo"/>
+                        <img src="/sotheby-logo.png" alt="Sotheby's International Realty logo" />
                         <div>
-                            <a href="phone:+504 9483-7049" title="Call West Bay">+504 9483-7049</a>
-                            <a href="mailto:golf@westbaygolfclubvillas.com" title="Contact West Bay">golf@westbaygolfclubvillas.com</a>
+                            <a href="phone:7132613315" title="Call Sotheby's International Realty">713 261 3315</a>
+                            <a href="phone:+50424453222" title="Call Sotheby's International Realty">+ 504 2445 3222</a>
+                            <a href="mailto:info@roatansir.com" title="Contact West Bay">info@roatansir.com</a>
                         </div>
                         <div>
-                            Palm Ln, West Bay, <br />
-                            Honduras
+                            Coral Stone Plaza<br />
+                            Sandy Bay Rd<br />
+                            Roatan, Honduras
                         </div>
                     </div>
 
@@ -37,33 +38,25 @@ const BookGolf = () => {
                         <div>
                             <label>
                                 Name
-                                <input type="text" name="name"/>
+                                <input type="text" name="name" />
                             </label>
                             <label>
                                 Email
-                                <input type="email" name="email"/>
+                                <input type="email" name="email" />
                             </label>
                             <label>
                                 Phone
-                                <input type="text" name="phone"/>
+                                <input type="text" name="phone" />
                             </label>
                         </div>
 
                         <div>
                             <label>
-                                Data
-                                <input type="date" name="date" />
-                            </label>
-                            <label>
-                                Name
-                                <input type="time" name="time" />
-                            </label>
-                            <label>
-                                Players number
-                                <input type="number" name="players" />
+                                Message
+                                <textarea name="message" id="" cols="30" rows="10"></textarea>
                             </label>
 
-                            <button>Contact booking</button>
+                            <button>Send message</button>
                         </div>
                     </form>
                 </div>
@@ -72,4 +65,4 @@ const BookGolf = () => {
     )
 }
 
-export default BookGolf
+export default RentalContact
