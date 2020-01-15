08150c25d6053ffe628d0ad19b91b713f491e251
holowzer <arthurlasnelhetic@gmail.com>
2020-01-15 23:08:31 +0100

    avancés

--- src/compfolder/Residences.jsx
+++ src/compfolder/Residences.jsx
@@ -20,9 +20,33 @@ const Residences = ()=>{
         <div className="residences-center-block">
              <h2>Conceived in the <br/>Heart of the Caribbean</h2>
         </div>
+        <div className="tooClassicDiv">
+        <div className="villaPreviewText">
+                <h2>make your mother proud</h2>
+                <p>we don't give a fuck about tomorrow. remember how we use to be. now i own my dreams between the lights of the tv. teenage headache dreams, i don't know how. i just took control in this latency of my memory.we don't give a fuck about tomorrow. remember how we use to be. now i own my dreams between the lights of the tv. teenage headache dreams, i don't know how. i just took control in this latency of my memory.
+                </p>
+            </div>
+
+            <div className="villaPreviewContainer">
+                <img src={img0} alt=""/>
+            </div>
+        </div>
+        <div className="lushTropcial">
+            <div className="lushPhoto">
+    <img src={img1} alt=""/>
+            </div>
+            <div className="lushText">
+            <h2>lush tropical damn !</h2>
+                <p>we don't give a fuck about tomorrow. remember how we use to be. now i own my dreams between the lights of the tv. teenage headache dreams, i don't know how. i just took control in this latency of my memory.we don't give a fuck about tomorrow. remember how we use to be. now i own my dreams between the lights of the tv. teenage headache dreams, i don't know how. i just took control in this latency of my memory.
+                </p>
+            </div>
+        </div>
+
+
+
+
         <GeneralMenu/>
         </div>
     )
-
 }
 export default Residences
