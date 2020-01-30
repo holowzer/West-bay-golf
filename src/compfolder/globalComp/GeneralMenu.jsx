cd51a8f15a7e5d406bfa374ecfe66fd6ec7855e4
Joshua <satwa.fr@gmail.com>
2020-01-30 18:49:13 +0100

    logo link to home

--- src/compfolder/globalComp/GeneralMenu.jsx
+++ src/compfolder/globalComp/GeneralMenu.jsx
@@ -15,40 +15,37 @@ const GeneralMenu = ()=>{
     
     return(
         <div className="generalMenu toggleMenu">
-                    <div className="topDiv">
-                        <img className="theCross" src={close} alt=""/>
-                    </div>
-                    <div className="midMenu">
-                        <div className="leftLinks">
-                        <Link to="/">
-                                <div href="">Homepage</div>
-                            </Link>
-                            <Link to="/golf">
-                                <div href="">Golf</div>
-                            </Link>
-                            <Link to="/residences">
-                            <div href="">The Residences</div>
-                            </Link>
-                            <Link to="/about-us">
-                                <div href="">About Us</div>
-                            </Link>
-
-
-
+            <div className="topDiv">
+                <Link to="/"><img className="theCross" src={close} alt="" /></Link>
             </div>
-                        <div className="rightLinks">
-                            <h3>Learn more about our villas</h3>
-                            <div className="whiteSotheby">
-                                <img src={sothebys} alt=""/>
-                            </div>
-                            <div className="contactCta">
-                            <Link to="#" onClick={() => {
-                            const menu = document.querySelector('.generalContactContainer')
-                            menu.classList.remove('toggleContact')
-                        }}>Contact Us</Link>
-                            </div>
-                        </div>
+            <div className="midMenu">
+                <div className="leftLinks">
+                <Link to="/">
+                    <div href="">Homepage</div>
+                </Link>
+                <Link to="/golf">
+                    <div href="">Golf</div>
+                </Link>
+                <Link to="/residences">
+                    <div href="">The Residences</div>
+                </Link>
+                <Link to="/about-us">
+                    <div href="">About Us</div>
+                </Link>
+            </div>
+            <div className="rightLinks">
+                <h3>Learn more about our villas</h3>
+                <div className="whiteSotheby">
+                    <img src={sothebys} alt=""/>
+                </div>
+                <div className="contactCta">
+                <Link to="#" onClick={() => {
+                    const menu = document.querySelector('.generalContactContainer')
+                    menu.classList.remove('toggleContact')
+                }}>Contact Us</Link>
                     </div>
+                </div>
+            </div>
         </div>
     )
 }
